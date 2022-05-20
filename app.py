c4d5969cbc9b94ba77ebea7007862e679e59bc26
Al-Mikan <nekomimi987@gmail.com>
2022-05-20 19:46:56 +0900

    a

--- app.py
+++ app.py
@@ -85,21 +85,24 @@ def handle_message(event):
     url = getpokebyid(event.message.text)
     print(event.source.user_id)
 
-    cur.execute('INSERT INTO zukan (line_id, poke_id) VALUES (1, 987);')
-    conn.commit()
+    # cur.execute("INSERT INTO zukan (line_id, poke_id) VALUES (1, 987);")
+    # conn.commit()
     
-    print('わーい')
+    # print('わーい')
 
-    with get_connection() as conn:
-        with conn.cursor(name='cs') as cur:
-            try:
-                sqlStr = "INSERT INTO zukan (line_id, poke_id) VALUES (1, 987);"
-                cur.execute(sqlStr)
-                # (mes,) = cur.fetchone()
-                # return mes
-            except:
-                mes = "exception"
-                return mes
+    sqlStr = "INSERT INTO zukan (line_id, poke_id) VALUES (1, 987);"
+    cur.execute(sqlStr)
+    
+    # with get_connection() as conn:
+    #     with conn.cursor(name='cs') as cur:
+    #         try:
+    #             sqlStr = "INSERT INTO zukan (line_id, poke_id) VALUES (1, 987);"
+    #             cur.execute(sqlStr)
+    #             # (mes,) = cur.fetchone()
+    #             # return mes
+    #         except:
+    #             mes = "exception"
+    #             return mes
 
     
     line_bot_api.reply_message(
